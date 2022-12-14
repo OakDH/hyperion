79210614368e58db85b1dd8e8a475c8d4365f766
nmohanu <nmohanu@gmail.com>
2022-12-14 15:06:12 +0100

    newscenes

--- src/main/java/io/github/oakdh/hyperion/SceneRegistry.java
+++ src/main/java/io/github/oakdh/hyperion/SceneRegistry.java
@@ -7,6 +7,8 @@ public class SceneRegistry
 {
     public static Scene LOGIN_SCENE = register(new BaseScene("login_scene"));
 
+    public static Scene WELCOME_SCENE = register(new BaseScene("welcome_scene"));
+
     public static Scene register(Scene toRegister)
     {
         return toRegister;
